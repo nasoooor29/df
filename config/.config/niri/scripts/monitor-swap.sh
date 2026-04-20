6b48602b243a30aa82ca17bfda2c5278072fadb3
Nasoooor <Nasoooor29@gmail.com>
2026-04-20 10:56:50 +0300

    update monitor swap

--- config/.config/niri/scripts/monitor-swap.sh
+++ config/.config/niri/scripts/monitor-swap.sh
@@ -1,12 +1,9 @@
-while true; do
-    if ! lsusb | grep -q 046d:c08b; then
-        niri msg output "eDP-1" on
-        niri msg output "DP-2" off
-        echo "Monitor disconnected"
-    else
-        niri msg output "eDP-1" off
-        niri msg output "DP-2" on
-        echo "Monitor connected"
-    fi
-    sleep 0.5
-done
+if ! lsusb | grep -q 046d:c08b; then
+    niri msg output "eDP-1" on
+    niri msg output "DP-1" off
+    echo "Monitor disconnected"
+else
+    niri msg output "eDP-1" off
+    niri msg output "DP-1" on
+    echo "Monitor connected"
+fi
